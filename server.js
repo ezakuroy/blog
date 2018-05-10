81f2f7659e77225af3a3c76221736cf79afd3223
ezakuroy <takumikh@gmail.com>
2018-05-10 00:27:59 -0400

    Added DB methods

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
+const MongoClient = require('mongodb').MongoClient
 
-app.listen(3000, function() {
-	console.log('test');
-});
+app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
+
+MongoClient.connect('mongodb://ezakuroy:Yamin7374!!!@ds119350.mlab.com:19350/ezaku', (err, client) => {
+	if (err) return console.log(err)
+	db = client.db('ezaku')
+	app.listen(3000, function() {
+		console.log('listening on 3000');
+	});
+})
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html')
+//	res.sendFile(__dirname + '/index.html')
+	var cursor = db.collection('posts').find().toArray((err, result) =>{
+		if(err) return console.log(err)
+
+		res.render('index.ejs', {posts: result})
+	})
 });
+
+app.get('/admin', function(req, res) {
+	res.sendFile(__dirname + '/admin.html')
+
+});
+
+app.post('/blogpost', (req, res) => {
+	db.collection('posts').save(req.body, (err, result) => {
+		if (err) return console.log(err)
+
+		console.log('saved to database')
+		res.redirect('/')
+	})
+})
